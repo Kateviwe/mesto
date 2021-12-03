f576dd29c6c8c2a4f237dd3722a1ca2830c70a93
Ekaterina <kateppol@yandex.ru>
2021-12-03 14:45:39 +0300

    feat: исправления внесены

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,41 @@
-const editButton = document.querySelector(".profile__button_edit");
-const popUp = document.querySelector(".popup");
-const exitButton = popUp.querySelector(".popup__exit");
-const noScroll = document.querySelector("body");
+//Поиск кнопок и попапа в DOM
+const editButton = document.querySelector('.profile__button_purpose_edit');
+const popUp = document.querySelector('.popup');
+const exitButton = popUp.querySelector('.popup__exit');
+//Поиск формы в DOM
+let formElement = popUp.querySelector('.popup__container');
+//Поиск полей формы в DOM
+let nameInput = popUp.querySelector('.popup__text_purpose_name');
+let jobInput = popUp.querySelector('.popup__text_purpose_characteristic');
+//Поиск в профиле имени и характеристики пользователя в DOM
+let person = document.querySelector('.profile__name');
+let characteristic = document.querySelector('.profile__characteristic');
 
-let formElement = popUp.querySelector(".popup__inner");
-let inputs = popUp.querySelectorAll(".popup__text");
-let nameInput = inputs[0];
-let jobInput = inputs[1];
-
+//При открытии модального окна данные из профиля записываются в форму
 function openPopup() {
-    popUp.classList.add("popup_opened");
-    noScroll.classList.add("page_hidden");
+    popUp.classList.add('popup_opened');
+    nameInput.value = person.textContent;
+    jobInput.value = characteristic.textContent;
 }
+
+//Закрытие модального окна
 function closePopup() {
-    popUp.classList.remove("popup_opened");
-    noScroll.classList.remove("page_hidden");
+    popUp.classList.remove('popup_opened');
 }
+
+//При нажатии кнопки 'Сохранить' данные из формы записываются в профиль и модальное окно закрывается
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
-    let name = document.querySelector(".profile__name");
-    let characteristic = document.querySelector(".profile__characteristic");
-
-    name.textContent = nameInput.value;
+    person.textContent = nameInput.value;
     characteristic.textContent = jobInput.value;
 
     closePopup();
 }
-nameInput.value = "Жак-Ив Кусто";
-jobInput.value = "Исследователь океана";
 
-editButton.addEventListener("click", openPopup);
-exitButton.addEventListener("click", closePopup);
-formElement.addEventListener("submit", formSubmitHandler);
+//Добавление слушателей событий
+editButton.addEventListener('click', openPopup);
+exitButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
 
 
